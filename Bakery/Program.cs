1e5df0a99fdaaf7de4e870abbd9c6c8c62331fad
Lindsay Warr <iamalion@gmail.com>
2023-07-14 10:51:42 -0700

    Add basic code to Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,5 @@
-// using System;
-// using Bakery.Models;
+using System;
+using Bakery.Models;
 
 namespace Bakery
 {
@@ -7,6 +7,16 @@ namespace Bakery
   {
     static void Main()
     {
+      Console.WriteLine("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread loaves cost $5 each");
+      Console.WriteLine("Pastries are $2 a piece");
+      Console.WriteLine("Today's Special: Buy 2 loaves get 1 free & Buy 3 pastries get 1 free");
+      Console.WriteLine("How many bread loaves would you like to order?");
+      string breadOrder = Console.ReadLine();
+      Console.WriteLine("How many pastries would you like to order?");
+      string pastryOrder = Console.ReadLine();
+
     
     }
   }
