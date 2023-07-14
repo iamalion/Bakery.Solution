93b8e6180b4cb6290997e7ac137f9a5a0ab8d8ae
Lindsay Warr <iamalion@gmail.com>
2023-07-14 08:56:31 -0700

    Pass test for pastry constructor

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -6,6 +6,12 @@ namespace Bakery.Tests
   [TestClass]
   public class PastryTests
   {
-    // Tests will go here!
+    [TestMethod]
+    
+        public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+        {
+            Pastry newPastry = new Pastry();
+            Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+        }
   }
 }
